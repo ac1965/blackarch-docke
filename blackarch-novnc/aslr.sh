62532e56fb9c2962f0e0b174218abc200653158c
YAMASHITA Takao <tjy1965@gmail.com>
2021-08-22 15:33:46 +0900

    update

--- blackarch-novnc/aslr.sh
+++ blackarch-novnc/aslr.sh
@@ -3,7 +3,7 @@
 file='/proc/sys/kernel/randomize_va_space'
 
 case "$1" in
-	on|1) echo 0 | sudo tee $file;;
+	on|0) echo 0 | sudo tee $file;;
 	off|2) echo 2 | sudo tee $file;;
 	*) echo "$0 [on|off]";;
 esac
